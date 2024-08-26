7b0ba4e4d5f2e3fa35812034ff94cb721a991943
deepakswins <deepakswins@gmail.com>
2024-08-26 22:03:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send('Hello we are testing!');
 });
 
-// Start the server on port 3000
+// Start the server on port 5000
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
